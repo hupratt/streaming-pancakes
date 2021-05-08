758362282e0207b16e2369e933a646db502bf4ac
hupratt <hpratt@gmail.com>
2021-05-08 20:32:57 +0200

    first commit

---
+++ src/stream/core/models.py
@@ -0,0 +1,22 @@
+from django.conf import settings
+from django.db import models
+
+
+class Category(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=64, unique=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Stream(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, related_name="streams", null=True
+    )
+    title = models.CharField(max_length=128)
+
+    def __str__(self):
+        return f"{self.title} | {self.user} | {self.category}"
---
+++ src/stream/stream/wsgi.py
@@ -0,0 +1,21 @@
+"""
+WSGI config for app project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os, dotenv
+
+from django.core.wsgi import get_wsgi_application
+
+
+if os.environ.get("DJANGO_DEVELOPMENT") == "True":
+    dotenv.read_dotenv(".env.development")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stream.settings")
+else:
+    dotenv.read_dotenv(".env")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stream.settings")
+application = get_wsgi_application()
