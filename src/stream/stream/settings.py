cd5c7a5455353d84e9486731c397a35c185a3960
hupratt <hprattdo@gmail.com>
2022-12-07 21:48:13 +0100

    2 fixes

--- src/stream/stream/settings.py
+++ src/stream/stream/settings.py
@@ -139,8 +139,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)), "static")
+MEDIA_ROOT = os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)), "media")
 
 CORS_ORIGIN_ALLOW_ALL = DEBUG
 
