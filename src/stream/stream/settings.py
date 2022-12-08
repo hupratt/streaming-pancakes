fdcd286e203cc0fec1d928e0be1f4fb98fee8c4e
hupratt <hprattdo@gmail.com>
2022-12-08 23:14:22 +0100

    debug cors

--- src/stream/stream/settings.py
+++ src/stream/stream/settings.py
@@ -149,7 +149,8 @@ CORS_ORIGIN_WHITELIST = [
     "http://127.0.0.1:8000",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "live.craftstudios.shop"
+    "live.craftstudios.shop",
+    "https://live.craftstudios.shop:444"
 ]
 
 
